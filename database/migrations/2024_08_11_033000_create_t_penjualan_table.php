8c72b1cf35a2630e2b00046e53a869fc4cbdd5b3
izmi <izmiukhti@gmail.com>
2024-08-11 10:33:45 +0700

    commit files

--- database/migrations/2024_08_11_033000_create_t_penjualan_table.php
+++ database/migrations/2024_08_11_033000_create_t_penjualan_table.php
@@ -12,8 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_penjualan', function (Blueprint $table) {
-            $table->id();
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('pembeli', 50);
+            $table->string('penjualan_kode', 20)->unique();
+            $table->dateTime('penjualan_tanggal');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
