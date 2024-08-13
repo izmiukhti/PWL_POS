a053a4ac4269d19201afe12513c10e76c78f2686
izmi <izmiukhti@gmail.com>
2024-08-14 04:24:52 +0700

    commit files

--- app/Http/Controllers/AnggotaController.php
+++ app/Http/Controllers/AnggotaController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Anggota;
 
 class AnggotaController extends Controller
 {
-    //
+    public function cekObject(){
+        $anggota = new Anggota;
+        
+        dump($anggota);
+    }
 }
