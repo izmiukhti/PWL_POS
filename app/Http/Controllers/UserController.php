f0acef6151644e3f552d69f08b285ae80c924b31
izmi <izmiukhti@gmail.com>
2024-08-14 04:16:40 +0700

    commit files

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // $data = [
+        //     'username'=>'izmi',
+        //     'nama'=>'izmi',
+        //     'password'=>Hash::make('123'),
+        //     'user_id'=>4,
+        //     'level_id'=>1
+        // ];
+        // UserModel::insert($data);
+
+        $data = [
+            'nama'=>'ukhti',
+        ];
+        //UserModel::where('username', 'izmi')->update($data);
+
+        UserModel::where('username', 'izmi')->delete();
+
         $user = UserModel::all();
         return view('user', ['data' => $user]);
-
     }
 }
