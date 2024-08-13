dd4db7afa17dde377c0d25a366420aca23470671
izmi <izmiukhti@gmail.com>
2024-08-14 02:13:43 +0700

    commit files

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,7 +9,10 @@ class LevelController extends Controller
 {
     public function index()
     {
-        DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-        return 'insert data baru berhasil';
+        //DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        //return 'insert data baru berhasil';
+
+        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        return 'Update data berhasil. Jumlah data yang di update : ' . $row . ' baris';
     }
 }
