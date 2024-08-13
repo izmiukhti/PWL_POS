fac05d0d0737100470c69eeb8f8ff06202e0ada8
izmi <izmiukhti@gmail.com>
2024-08-14 03:29:04 +0700

    commit files

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -19,5 +19,8 @@ class KategoriController extends Controller
 
         //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Cemilan']);
         //return 'update data berhasil, jumlah data yang terupdate' . $row . ' baris';
+        
+        $data=DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        return 'delete data berhasil, jumlah data yang terhapus ' . $data . ' baris';
     }
 }
