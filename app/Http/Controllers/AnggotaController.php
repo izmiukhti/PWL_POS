512d7bdbcaa1ca23ca8db2dd6ae555ae0f65c333
izmi <izmiukhti@gmail.com>
2024-08-14 11:10:31 +0700

    commit files

--- app/Http/Controllers/AnggotaController.php
+++ app/Http/Controllers/AnggotaController.php
@@ -8,9 +8,42 @@ use App\Models\Anggota;
 
 class AnggotaController extends Controller
 {
-    public function cekObject(){
+    public function cekObject()
+    {
         $anggota = new Anggota;
-        
         dump($anggota);
+    }
+
+    // public function insert()
+    // {
+    //     $anggota = new Anggota;
+    //     $anggota->nip = '234';
+    //     $anggota->nama = 'ukhti';
+    //     $anggota->tanggal_lahir = '2002-06-25';
+    //     $anggota->nilai = '3.7';
+    //     $anggota->save();
+        
+    //     echo "DATA BERHASIL DISIMPAN";
+    // }
+
+    // public function update()
+    // {
+    //     $anggota = Anggota::find(1);
+    //     $anggota->nama = 'ukhti';
+    //     $anggota->nilai = '3.5';
+    //     $anggota->save();
+    //     echo "data berhasil diupdate";
+    // }
+
+    // public function delete()
+    // {
+    //     $anggota = Anggota::find(1);
+    //     $anggota->delete();
+    //     echo "data berhasil di hapus";
+    // }
+
+    public function all(){
+        $result= Anggota::all();
+        return view('tampilkan_anggota',['anggota'=>$result]);
     }
 }
